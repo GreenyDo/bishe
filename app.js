0ed35abbc734457795d61e06ac8f327d2276001a
GreenyDo <GreenyDo>
2017-04-29 14:07:56 +0800

    change_password.html compete

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -23,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/', users);
+app.use("/",users);
+app.use("/",index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-
+router.get('/ok', function(req, res, next) {
+  res.send("ok");
+});
 
 module.exports = router;
